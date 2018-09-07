44693466d05c33f27f91ce8ea55217b7f8103f80
ruanvivian <42475800+ruanvivian@users.noreply.github.com>
2018-09-07 15:32:33 -0300

    add

---
+++ artistas.php
@@ -0,0 +1,89 @@
+<?php 
+    class Artistas{
+     private $nome;
+     private $genero;
+     private $biografia;
+     private $img;
+     private $sexo;
+     private $revelacao;
+     private $recomendar;
+     private $live;
+     private $menu;
+        
+        public function getNome(){
+            return $this->nome;
+        }
+        
+        
+        public function getGenero(){
+            return $this->genero;
+        }
+        
+        
+        public function getBiografia(){
+            return $this->biografia;
+        }
+        
+        
+        public function getSexo(){
+            return $this->sexo;
+        }
+        
+        
+        public function getImg(){
+            return $this->img;
+        }
+        
+        public function getRevelacao(){
+            return $this->revelacao;
+        }
+        
+        public function getRecomendar(){
+            return $this-> recomendar;
+        }
+        
+        public function getLive(){
+            return $this->live;
+        }
+      
+        public function getMenu(){
+            return $this->menu;
+        }
+      
+      
+      
+      
+      
+        public function setNome($a){
+             $this->nome = $a;
+        }
+        
+        public function setGenero($a){
+             $this->genero = $a;
+        }
+        
+        public function setBiografia($a){
+             $this->biografia = $a;
+        }
+        public function setSexo($a){
+             $this->sexo = $a;
+        }
+        
+        public function setImg($a){
+             $this->img = $a;
+        }
+        public function setRevelacao($a){
+             $this->revelacao = $a;
+        }
+        public function setRecomendar($a){
+             $this->recomendar = $a;
+        }
+        public function setLive($a){
+             $this->live = $a;
+        }
+        public function setMenu($a){
+             $this->menu = $a;
+        }
+    }
+
+?>
---
+++ modelArtistas.php
@@ -0,0 +1,111 @@
+<?php
+include 'artistas.php';
+
+class ModelArtistas
+{
+
+    public function adicionar(artistas $artistas){
+        include 'bd.php';
+        
+        
+        $query = "INSERT INTO artistas(nome, genero, biografia, img, sexo, revelacao, menu, live, recomendar) 
+            VALUES (:nome, :genero, :biografia, :img, :sexo, :revelacao, :menu, :live, :recomendar)";
+        
+        $statement= $connection->prepare($query);
+        
+        $valores = array();
+        $valores[':nome'] = $user->getNome();
+        $valores[':genero'] = $user->getGenero();
+        $valores[':biografia'] = $user->getBiografia();
+        $valores[':img'] = $user->getImg();
+        $valores[':sexo'] = $user->getSexo();
+        $valores[':revelacao'] = $user->getRevelacao();
+        $valores[':menu'] = $user->getMenu();
+        $valores[':live'] = $user->getLive();
+        $valores[':recomendar'] = $user->getRecomendar();
+
+        
+        $result = $statement->execute($valores);
+        
+        if(  empty($result)  ){
+            echo "Inserir artista deu erro.";
+        }else{
+            echo "Inserir artista deu certo.";
+        } 
+    }
+
+    public function listar(){
+        include 'bd.php';
+        
+        $query = "SELECT nome, genero, biografia, img, sexo, revelacao, menu, live, recomendar FROM artistas";
+        
+        $statement = $connection->prepare($query);
+        
+        //executar a query
+        $result = $statement->execute();
+        
+        //junta todos os usuarios retornados em um array de array usuario
+        //pro exemplo o 
+        $u = $result[0];//pega primeiro usuario
+        echo $u['nome'];
+        $u = $result[1];
+        echo $u['nome'];//pega segundo usuario
+        
+        $result = $statement->fetchAll();
+        
+        return $result;
+    }
+
+    
+    public function editar(artistas $artistas){
+        include 'bd.php';
+        
+        
+        $query = "UPDATE artistas SET nome = :nome, genero = :genero, biografia = :biografia, img = :img, sexo = :sexo, revelacao = :revelacao, menu = :menu, live = :live, recomendar WHERE nome = :nome";
+        
+        $statement= $connection->prepare($query);
+        
+        $valores = array();
+        $valores[':nome'] = $user->getNome();
+        $valores[':genero'] = $user->getGenero();
+        $valores[':biografia'] = $user->getBiografia();
+        $valores[':img'] = $user->getImg();
+        $valores[':sexo'] = $user->getSexo();
+        $valores[':revelacao'] = $user->getRevelacao();
+        $valores[':menu'] = $user->getMenu();
+        $valores[':live'] = $user->getLive();
+        $valores[':recomendar'] = $user->getRecomendar();
+        
+        $result = $statement->execute($valores);
+        
+        if(  empty($result)  ){
+            echo "Alterar usuário deu erro.";
+        }else{
+            echo "Alterar usuário deu certo.";
+        } 
+    }
+    
+    public function remover($nome){
+        include 'bd.php';
+        
+        $query = "DELETE FROM artistas WHERE nome = :nome";
+        
+        $statement = $connection->prepare($query);
+        
+        
+        $valores = array();
+        $valores[':nome'] = $nome;
+    
+        $result = $statement->execute($valores);
+        
+        
+        if(  empty($result)  ){
+            echo "Remover artistas deu erro.";
+        }else{
+            echo "Remover artistas deu certo.";
+        }         
+        
+    }
+}
+
+?>
